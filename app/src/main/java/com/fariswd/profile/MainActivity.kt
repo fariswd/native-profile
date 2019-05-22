eb198b0a1900aea860a5def59f1bdc7f0ab04151
Faris Widyantho <radenfaris@gmail.com>
2019-05-22 23:23:56 +0700

    init

---
+++ app/src/main/java/com/fariswd/profile/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.fariswd.profile
+
+import android.opengl.Visibility
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textMessage: TextView
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                textMessage.setText(R.string.title_home)
+                dashboardSetVisibility(true)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                textMessage.setText(R.string.title_dashboard)
+                dashboardSetVisibility(false)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                textMessage.setText(R.string.title_notifications)
+                dashboardSetVisibility(false)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        textMessage = findViewById(R.id.message)
+        dashboardSetVisibility(true)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+
+    fun dashboardSetVisibility(typeofVisiblity: Boolean) {
+        viewDashboard.visibility = if(typeofVisiblity) View.VISIBLE else View.INVISIBLE
+    }
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Native Profile
+WIP Layouting xml android native-kotlin
+
+## Mockup
+by: ?? https://id.pinterest.com/pin/678565868834917992/ 
+
+## SS
+<img src="https://raw.githubusercontent.com/fariswd/native-profile/master/ss.png" height="300">
+
+
+fariswd  
+2019
